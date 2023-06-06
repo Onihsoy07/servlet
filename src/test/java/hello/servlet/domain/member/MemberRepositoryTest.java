1df8312f5d1dcdf19f5e41aa83b20b5a82730ec1
Onihsoy07 <84126411+Onihsoy07@users.noreply.github.com>
2023-06-06 22:50:06 +0900

    Member, MemberRepository 구현 및 테스트

---
+++ src/test/java/hello/servlet/domain/member/MemberRepositoryTest.java
@@ -0,0 +1,53 @@
+package hello.servlet.domain.member;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+class MemberRepositoryTest {
+
+    private final MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @AfterEach
+    void afterEach() {
+        memberRepository.clearStore();
+    }
+
+    @Test
+    void save() {
+        //given
+        Member member = new Member("park", 20);
+
+        //when
+        Member savedMember = memberRepository.save(member);
+
+        //then
+        Member findMember = memberRepository.findById(savedMember.getId());
+        assertThat(findMember).isEqualTo(savedMember);
+    }
+
+    @Test
+    void findAll() {
+        //given
+        Member member1 = new Member("member1", 20);
+        Member member2 = new Member("member2", 22);
+        Member member3 = new Member("member3", 27);
+
+        memberRepository.save(member1);
+        memberRepository.save(member2);
+        memberRepository.save(member3);
+
+        //when
+        List<Member> members = memberRepository.findAll();
+
+        //then
+        assertThat(members.size()).isEqualTo(3);
+        assertThat(members).contains(member1, member2, member3);
+    }
+
+}
