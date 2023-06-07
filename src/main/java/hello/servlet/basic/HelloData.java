8a117421d47fe7b7fa6699ce1a1c2c7ca9b9a5b2
Onihsoy07 <84126411+Onihsoy07@users.noreply.github.com>
2023-06-07 22:57:59 +0900

    MemberFormServlet, MemberSaveServlet 구현

--- src/main/java/hello/servlet/basic/HelloData.java
+++ src/main/java/hello/servlet/basic/HelloData.java
@@ -1,8 +1,12 @@
 package hello.servlet.basic;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class HelloData {
     private String username;
     private int age;
