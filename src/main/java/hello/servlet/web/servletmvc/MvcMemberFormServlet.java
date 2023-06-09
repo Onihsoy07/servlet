9a3ac3f66b9b25edcb65ef68ae9c14c3cfc86d4a
Onihsoy07 <84126411+Onihsoy07@users.noreply.github.com>
2023-06-09 21:59:41 +0900

    java 코드로 MVC 패턴 만들기

---
+++ src/main/java/hello/servlet/web/servletmvc/MvcMemberFormServlet.java
@@ -0,0 +1,22 @@
+package hello.servlet.web.servletmvc;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "mvcMemberFormServlet", urlPatterns = "/servlet-mvc/members/new-form")
+public class MvcMemberFormServlet extends HttpServlet {
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String viewPath = "/WEB-INF/views/new-form.jsp";
+        RequestDispatcher dispatcher = request.getRequestDispatcher(viewPath);
+        //redirect랑 비슷하지만 foward는 서버 내부에서 요청(클라이언트는 모름)
+        //redirect는 클라이언트가 수행
+        dispatcher.forward(request, response);
+    }
+}
